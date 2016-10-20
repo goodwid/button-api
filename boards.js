b417a9717dfab2a10bd5d808bf3ef5ba50980746
David Goodwin <goodwid@gmail.com>
2016-10-20 16:40:41 -0700

    Crap, what did I change?

--- boards.js
+++ boards.js
@@ -1,10 +1,6 @@
 var boards = {
-  featherHuzzah: {
-    buttons: [0,2,4,5,12,13,14,15]
-  },
-  NodeMCU: {
-    buttons: [2, 4]
-  }
+  featherHuzzah: [0,2,4,5,12,13,14,15]
+  // NodeMCU: [D2, D4]
 };
 
 module.exports = function(board) {
--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var config = require('./config')();
-var board = require('./boards');
-var buttons = board(config.board).buttons;
+var board = require('./boards')
+var buttons = board(config.board);
 
 var http = require('http');
 var wifi = require('Wifi');
 var options = {
   repeat: true,
   edge: 'rising'
-}
+};
+
 wifi.connect(config.ssid, {password:config.wifiPw}, function(err){
   if (err) {
-    return console.log('error: ', err);
+    return console.log('wifi connect error: ', err);
   }
-  console.log('Connection info: ', wifi.getIP());
+  console.log('Wireless network connection successful!\n * IP Address is', wifi.getIP().ip);
 });
 
 function handler (e) {
@@ -28,6 +29,7 @@ function handler (e) {
 }
 
 buttons.forEach(function(pin) {
+  console.log('registering pin '+ pin);
   pinMode(pin, 'input_pullup');
   setWatch(handler, pin, options);
 });
